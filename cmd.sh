4238ec878509dc4ef854eb4909ca3937acd34bda
Clara <0310qian@gmail.com>
2026-02-09 22:52:37 +0800

    train G&GFood

---
+++ cmd.sh
@@ -0,0 +1,2 @@
+0209-192154-Grocery_and_Gourmet_Food-G&G_Food_first_train | main.py --data_name=Grocery_and_Gourmet_Food --max_seq_length=50 --hidden_size=64 --batch_size=256 --msg=G&G_Food_first_train --scheduler warmup+multistep --milestones [25, 50] --loader_type new --gcn_mode batch --do_eval --do_test
+
